90195324dfcf2b3758996b2789c2cfb3ebab5c35
remone2 <jamodinm@yahoo.com>
2021-12-19 01:47:39 -0600

    add render trip and plan trip function

--- src/app.js
+++ src/app.js
@@ -60,3 +60,73 @@ function renderDestination(data) {
     );
   });
 }
+
+function planTrip() {
+  let firstInput = document.getElementsByClassName("origins")[0];
+  let secondInput = document.getElementsByClassName("destinations")[0];
+  let options1Li = firstInput.querySelectorAll("li");
+  let options2Li = secondInput.querySelectorAll("li");
+  for (option of options1Li) {
+    if (option.classList.contains("selected")) {
+      lat1 = option.getAttribute("data-lat");
+      lon1 = option.getAttribute("data-long");
+    }
+  }
+  for (option of options2Li) {
+    if (option.classList.contains("selected")) {
+      lat2 = option.getAttribute("data-lat");
+      lon2 = option.getAttribute("data-long");
+    }
+  }
+  getTrip(
+    `https://api.winnipegtransit.com/v3/trip-planner.json?api-key=FYkqe4oC7kGOnfP30xPB&origin=geo/${lat1},${lon1}&destination=geo/${lat2},${lon2}`
+  )
+    .then((data2) => renderTrip(data2))
+    .catch((err) => console.log(err));
+}
+
+function renderTrip(data) {
+  let output = document.getElementsByClassName("my-trip")[0];
+  while (output.hasChildNodes()) {
+    output.removeChild(output.firstChild);
+  }
+  let a = 0;
+  data.plans.forEach((info) => {
+    output.insertAdjacentHTML("beforeend", `<h2>Route ${a + 1}</h2>`);
+    a++;
+    for (let i = 0; i < info.segments.length; i++) {
+      if (info.segments[i].type === "walk") {
+        output.insertAdjacentHTML(
+          "beforeend",
+          `<li><i class="fas fa-walking" aria-hidden="true"></i>
+            ${info.segments[i].type} for ${
+            info.segments[i].times.durations.total
+          } minutes to ${
+            !info.segments[i].to.stop
+              ? "destination"
+              : info.segments[i].to.stop.name
+          }
+          </li>`
+        );
+      }
+      if (info.segments[i].type === "ride") {
+        output.insertAdjacentHTML(
+          "beforeend",
+          `<li><i class="fas fa-bus" aria-hidden="true"></i>
+            ${info.segments[i].type} the ${info.segments[i].route.name} bus for ${info.segments[i].times.durations.total} minutes
+          </li>`
+        );
+      }
+      if (info.segments[i].type === "transfer") {
+        output.insertAdjacentHTML(
+          "beforeend",
+          `<li><i class="fas fa-ticket-alt" aria-hidden="true"></i>
+            ${info.segments[i].type} from ${info.segments[i].from.stop.name} to
+            ${info.segments[i].to.stop.name}
+          </li>`
+        );
+      }
+    }
+    output.insertAdjacentHTML("beforeend", `<hr>`);
+  });
+}
